fff18405d0a5d1ac19f66817b7fd33ab147b5fa5
joewalker <assassin.joe@hotmail.com>
2020-09-03 11:24:30 +0700

    add line nofity channel

--- channels/grafana/handler.go
+++ channels/grafana/handler.go
@@ -1,19 +1,95 @@
 package grafana
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/joecomscience/prom-webhook/notify_channel"
+	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func Handler(w http.ResponseWriter, r *http.Request)  {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Fatal(err)
+func Handler(w http.ResponseWriter, r *http.Request) {
+	b, _ := ioutil.ReadAll(r.Body)
+	fmt.Println(string(b))
+
+	payload := Payload{}
+	if err := json.NewDecoder(r.Body).Decode(&payload); err != nil {
+		fmt.Printf("Decode info to struct error: %v\n", err.Error())
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(string(body))
+
+	defer r.Body.Close()
+	imageFileName := payload.GetImageFileName()
+	if err := payload.DownloadImage(imageFileName); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	lineManager := notify_channel.LineManager{
+		Message:   payload.Message,
+		ImageFile: imageFileName,
+	}
+
+	if err := lineManager.Send(); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err := payload.RemoveImageFile(imageFileName); err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
+}
+
+type Payload struct {
+	DashboardID int `json:"dashboardId"`
+	EvalMatches []struct {
+		Value  int         `json:"value"`
+		Metric string      `json:"metric"`
+		Tags   interface{} `json:"tags"`
+	} `json:"evalMatches"`
+	ImageURL string `json:"imageUrl"`
+	Message  string `json:"message"`
+	OrgID    int    `json:"orgId"`
+	PanelID  int    `json:"panelId"`
+	RuleID   int    `json:"ruleId"`
+	RuleName string `json:"ruleName"`
+	RuleURL  string `json:"ruleUrl"`
+	State    string `json:"state"`
+	Tags     struct {
+	} `json:"tags"`
+	Title string `json:"title"`
+}
+
+func (payload Payload) DownloadImage(filename string) error {
+	resp, err := http.Get(payload.ImageURL)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	output, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer output.Close()
+
+	_, err = io.Copy(output, resp.Body)
+	return err
+}
+
+func (payload Payload) RemoveImageFile(filename string) error {
+	return os.Remove(filename)
+}
+
+func (payload Payload) GetImageFileName() string {
+	filenames := strings.Split(payload.ImageURL, "/")
+	lastIndex := len(filenames) - 1
+	return filenames[lastIndex]
 }
--- notify_channel/line.go
+++ notify_channel/line.go
@@ -1,1 +1,56 @@
 package notify_channel
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
+type LineManager struct {
+	Message   string
+	ImageFile string
+}
+
+func (manager LineManager) Send() error {
+	lineAPIUrl := os.Getenv("LINE_URL")
+	lineToken := os.Getenv("LINE_TOKEN")
+	imageFile := fmt.Sprintf("./%s", manager.ImageFile)
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	_ = writer.WriteField("message", "joewalker")
+
+	file, err := os.Open(imageFile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	part2, err := writer.CreateFormFile("imageFile", filepath.Base(imageFile))
+	_, err = io.Copy(part2, file)
+	if err != nil {
+		return err
+	}
+
+	err = writer.Close()
+	if err != nil {
+		return err
+	}
+
+	client := &http.Client{}
+	req, _ := http.NewRequest(http.MethodPost, lineAPIUrl, payload)
+	req.Header.Add("Content-Type", writer.FormDataContentType())
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", lineToken))
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	fmt.Println(string(body))
+	return err
+}
